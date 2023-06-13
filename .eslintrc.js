219ca0e9fca476b5f95fa93ed8304c19e405548a
Lexuil <ojumanab@unal.edu.co>
2023-06-13 15:43:20 -0500

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard-with-typescript',
+    'plugin:vue/vue3-essential',
+    '@nuxt/eslint-config-typescript'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+  }
+}
---
+++ composables/useMeta.ts
@@ -0,0 +1,14 @@
+export default function () {
+  useSeoMeta({
+    title: 'Dall-e App',
+    description: 'A simple app to use openai\'s Dall-e',
+    ogTitle: 'Dall-e App',
+    ogDescription: 'A simple app to use openai\'s Dall-e'
+  })
+
+  useHead({
+    htmlAttrs: {
+      lang: 'en'
+    }
+  })
+}
